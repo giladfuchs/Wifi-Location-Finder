2b69ac7c073fe42d0be297aa1c50f68108c82ec4
מתן <@matan>
2017-12-21 17:45:32 +0200

    1

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -14,14 +14,16 @@ public class Main {
 		 * @param srcPath get the path to upload the csv file we create 
 		 */
 		String dirPath = "C:/OOP/WigleWifi_files";
-		String desPath = "C:/OOP/List_of_wifi.csv";
-       
-		Q2 q2 = new Q2();          
+		String desPath = "C:/OOP/List_of_wifi.csv";		          
 		/**
 		 *  read files from directory and write to file
 		 *  @param b get true while the function create output file
 		 */
+		Q2 q2 = new Q2();
 		boolean b = q2.ReadDir(dirPath,desPath);
+		/**
+		 *  read file -> filter by time&ID&location -> filter by MAC -> write to kml
+		 */
 		if(b){
 			String filePath = "C:/OOP/List_of_wifi.csv";
 			String desPathAfterFilterCSV = "C:/OOP/List_of_wifi_after_filter.csv";
@@ -29,8 +31,6 @@ public class Main {
 			Q3 q3 = new Q3();
 			q3.ReadFile(filePath,desPathAfterFilterCSV,desPathAfterFilterKML);     
 		}
-		/**
-		 *  read file -> filter by time&ID&location -> filter by MAC -> write to kml
-		 */
+		
 	}
 }
