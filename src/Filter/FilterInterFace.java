cb8e5ecd8f6a61d82e50a5345b4652ee7f93b25e
igilfu <33724031+igilfu@users.noreply.github.com>
2017-12-28 19:31:21 +0200

    aa

---
+++ src/Filter/FilterInterFace.java
@@ -0,0 +1,12 @@
+package Filter;
+
+import java.util.List;
+
+import Objects.Row;
+
+public interface FilterInterFace {
+	public  List<Row> CalculateByLocation1(List<Row> listInput, List<Row> listOutput,  double Lon ,double Lat,double Radius) ;
+	public List<Row> CalculateByID1(List<Row> listInput,List<Row> listOutput,String id);
+	public List<Row> CalculateByTime1(List<Row> listInput, List<Row> listOutput,String startDate,String endDate);
+
+}
