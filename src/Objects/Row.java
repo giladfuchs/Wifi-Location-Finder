15c0dbccebfa49cf0fb5656fab728a39c910bab9
igilfu <33724031+igilfu@users.noreply.github.com>
2018-01-07 23:54:19 +0200

    Last Update of exercise 3

--- src/Objects/Row.java
+++ src/Objects/Row.java
@@ -49,18 +49,22 @@ public class Row
 	public String toString() {
 		return "Row [element=" + element + ", head=" + head + "]";
 	}
-/**
- * Checking if the Row is duplicate
- * @param another
- * @return
- */
+	/**
+	 * Checking if the Row is duplicate
+	 * @param another
+	 * @return
+	 */
 	public boolean isEquals(Row another)
 	{
 		if(this.getHead().getID().equals(another.getHead().getID()) && 
 				this.getHead().getTime().equals(another.getHead().getTime()) && 
 				this.getHead().getLat().equals(another.getHead().getLat()) &&
 				this.getHead().getLon().equals(another.getHead().getLon()) &&
-				this.getHead().getAlt().equals(another.getHead().getAlt()))
+				this.getHead().getAlt().equals(another.getHead().getAlt()) &&
+				this.getElement().get(0).getChanal().equals(another.getElement().get(0).getChanal()) &&
+				this.getElement().get(0).getMac().equals(another.getElement().get(0).getMac()) &&
+				this.getElement().get(0).getSignal().equals(another.getElement().get(0).getSignal()) &&
+				this.getElement().get(0).getSsid().equals(another.getElement().get(0).getSsid()))
 			return true;
 		return false;
 	}
